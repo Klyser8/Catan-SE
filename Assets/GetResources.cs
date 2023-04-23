30dac8b2655b3e77ec97a793ba0d730e2ec05c46
egormis <91596001+egormis@users.noreply.github.com>
2023-04-23 17:52:55 +0100

    DevCard
    new panel for active dev card so player can read it and decide play it or return to the hand

---
+++ Assets/GetResources.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetResources : MonoBehaviour
+{
+    public GameObject hand;
+
+    public GameObject[] resources;
+
+// Brick 0
+// Ore 1
+// Sheep 2
+// Wheat 3
+// Wood 4
+
+    public void TakeResources(int numberOfResources, int indexOfResource) {
+        for (int i = 0; i < numberOfResources; i++) {
+            GameObject newResource = Instantiate(resources[indexOfResource]);
+            newResource.transform.SetParent(hand.transform);
+        }
+    } 
+
+}
